2f1bf48d7a4a38e2e55840e3c1b66237a3bf9795
Lorenz Kleissner <lorenz.kleissner@hotmail.com>
2012-10-27 17:06:51 +0200

    Minor Source Updates

--- gen/net/gomonkey/R.java
+++ gen/net/gomonkey/R.java
@@ -22,7 +22,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int brownred=0x7f040000;
+        public static final int brown=0x7f040000;
         public static final int welcome_text=0x7f040002;
     }
 }
