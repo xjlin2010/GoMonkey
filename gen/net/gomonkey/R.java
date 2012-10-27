e3924e540ed39c0558b633606651ca8379b5aaf9
Lorenz Kleissner <lorenz.kleissner@hotmail.com>
2012-10-27 16:45:32 +0200

    Updated Source

--- gen/net/gomonkey/R.java
+++ gen/net/gomonkey/R.java
@@ -14,12 +14,15 @@ public final class R {
         public static final int bg=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
+    public static final class id {
+        public static final int textView1=0x7f050000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int start_game=0x7f040002;
+        public static final int brownred=0x7f040000;
+        public static final int welcome_text=0x7f040002;
     }
 }
--- src/net/gomonkey/StartUpActivity.java
+++ src/net/gomonkey/StartUpActivity.java
@@ -2,6 +2,8 @@ package net.gomonkey;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartUpActivity extends Activity {
     /** Called when the activity is first created. */
