546a7e0d5251e3eca00c7de635272ebcd2679e02
Lorenz Kleissner <lorenz.kleissner@hotmail.com>
2012-10-27 15:24:06 +0200

    Uploaded Source

---
+++ src/net/gomonkey/StartUpActivity.java
@@ -0,0 +1,13 @@
+package net.gomonkey;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartUpActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
