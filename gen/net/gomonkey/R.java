64a371fcc896bf2049606871e2b5abbca1fb9842
Lorenz Kleissner <lorenz.kleissner@hotmail.com>
2012-10-28 15:04:20 +0100

    Big Fat Source Update
    Credits fully working now

--- gen/net/gomonkey/R.java
+++ gen/net/gomonkey/R.java
@@ -19,11 +19,12 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int button_about=0x7f060003;
-        public static final int button_credits=0x7f060004;
-        public static final int button_start=0x7f060002;
-        public static final int copyright=0x7f060000;
-        public static final int welcome_message=0x7f060001;
+        public static final int button_about=0x7f060004;
+        public static final int button_credits=0x7f060005;
+        public static final int button_start=0x7f060003;
+        public static final int copyright=0x7f060001;
+        public static final int textView1=0x7f060000;
+        public static final int welcome_message=0x7f060002;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -36,6 +37,7 @@ public final class R {
         public static final int button_credits=0x7f050005;
         public static final int button_start=0x7f050003;
         public static final int copyright=0x7f050002;
+        public static final int credits=0x7f050006;
         public static final int welcome_text=0x7f050001;
     }
 }
